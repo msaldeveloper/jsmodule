c412585a62b879f733843fa5501dac981ba580b4
msalda <msalda53@gmail.com>
2020-12-07 22:20:00 -0600

    first class code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 console.log("hello koders")
+
+/*  variables var , Let , Const
+ let si va a cambiar mi valor
+ const si es un valor fijo
+ snake_case
+ Const
+ const NAME_KODER = ‘oscar’
+ NAME_KODER = ‘Oscar’
+ let
+ let name_koder = ‘’
+ Case Styles
+ camelCase
+ PascalCase
+ snake_case */
+
+ let name_koder = prompt('¿cual es tu nombre?')
+ console.log(name_koder)
+ let age_koder = prompt('¿cual es tu edad?')
+ console.log(age_koder)
+ let resultSuma = parseInt(age_koder) + 5
+console.log('suma', resultSuma)
+console.log('suma', age_koder * 5)
+console.log('suma', age_koder / 5)
+console.log('suma', age_koder - 5)
