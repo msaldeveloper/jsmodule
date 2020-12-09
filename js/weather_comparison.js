4e9ab16fd007327a09d57ab410eafb9f6ee8bf84
msalda <msalda53@gmail.com>
2020-12-08 21:40:40 -0600

    add switch comp

--- js/weather_comparison.js
+++ js/weather_comparison.js
@@ -11,7 +11,7 @@
 */
 alert("opciones de clima ##SOLEADO##    ##LLUVIOSO##    ##NEVANDO##    ##NUBLADO##    ##OTRO## ")
 let weather =prompt("ingresa como esta el clima")
-if(weather==="soleado"){
+/* if(weather==="soleado"){
     console.log("el dia esta soleado sal a dar un paseo")
 }else if(weather==="lluvioso"){
     console.log("el dia esta lluvioso sera mejor que salgas con paraguas")
@@ -24,7 +24,28 @@ else if(weather==="nublado"){
 }
 else{
     alert("el clima es diferente?")
-    weather = prompt("ingresa una descripcion del clima")
-    console.log(weather)
+    let weather_description = prompt("ingresa una descripcion del clima")
+    console.log(weather_description)
+
+}
+ */
+switch(weather){
+    case "soleado":
+        console.log("el dia esta soleado sal a dar un paseo")
+        break
+
+    case "lluvioso":
+        console.log("el dia esta lluvioso sera mejor que salgas con paraguas")
+        break
+    case "nevando":
+        console.log("el dia esta nevando ponte un abrigo")
+        break
+    case "nublado":
+        console.log("el dia esta nublado mejor abrigate y llevate un paraguas")
+        break
+    default:
+        alert("el clima es diferente?")
+        let weather_description = prompt("ingresa una descripcion del clima")
+        console.log(weather_description)
 
 }
