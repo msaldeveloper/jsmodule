bed5d7923ed25d540ffee7b1456dbfeeb408c95c
msalda <msalda53@gmail.com>
2020-12-08 19:25:48 -0600

    weight in moon

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ console.log("hello koders")
  PascalCase
  snake_case */
 
- let name_koder = prompt('¿cual es tu nombre?')
+/*  let name_koder = prompt('¿cual es tu nombre?')
  console.log(name_koder)
  let age_koder = prompt('¿cual es tu edad?')
  console.log(age_koder)
@@ -22,4 +22,8 @@ console.log("hello koders")
 console.log('suma', resultSuma)
 console.log('suma', age_koder * 5)
 console.log('suma', age_koder / 5)
-console.log('suma', age_koder - 5)
+console.log('suma', age_koder - 5) */
+let weight_koder = prompt('¿cual es tu peso?')
+console.log(weight_koder)
+let moon_weight = ((weight_koder/9.81)*1.622)
+alert("tu peso en la luna es "+ moon_weight)
---
+++ js/weight_in_moon.js
@@ -0,0 +1,4 @@
+let weight_koder = prompt('¿cual es tu peso?')
+console.log(weight_koder)
+let moon_weight = ((weight_koder/9.81)*1.622)
+alert(moon_weight)
